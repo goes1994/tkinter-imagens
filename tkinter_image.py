5c4cdbce408738b40ca1064db6d23d1b01598b29
Lucas Goes <lucas.goes1994@gmail.com>
2023-11-17 20:56:08 -0300

    incio do codigo

--- tkinter_image.py
+++ tkinter_image.py
@@ -2,13 +2,30 @@ from tkinter import *
 
 from PIL import ImageTk, Image
 
+import os 
+
 root = Tk()
 
-img = Image.open('meme.jpg')
+#Lista os arquivos da pasta imagens
+arquivos = os.listdir('imagens')
 
-img_tk = ImageTk.PhotoImage(img)
+#Variavel para armazenar as imagens
+imagens = []
 
-img_label = Label(root, image=img_tk)
+#Percorer a lista de arquivos
+for arquivo in arquivos:
+    #Abre a imagem
+    img = Image.open('imagens/' + arquivo)
+    #Adiciona a imagem a lista
+    imagens.append(ImageTk.PhotoImage(img))
+
+#Exibe arquivos em um label
+#arquivos_label = Label(root, text=arquivos)
+#arquivos_label.pack()
+#img = Image.open('imagens/meme.jpg')
+#img_tk = ImageTk.PhotoImage(img)
+
+img_label = Label(root, image=imagens [0])
 
 img_label.pack()
 
---
+++ ze_do_codigo.py
@@ -0,0 +1,15 @@
+from tkinter import*
+
+root= Tk()
+
+root.title("ZE DO CODIGO")
+
+texto =Entry(root, width=60)
+
+texto.pack()
+
+botao = Button(root, text='CLICA AQUI')
+
+botao.pack()
+
+root.mainloop()
