bf2753b6f3adb25540d2bbfd481e5a89b0036529
Lucas Goes <lucas.goes1994@gmail.com>
2023-11-22 20:47:48 -0300

    listar imagens

--- tkinter_image.py
+++ tkinter_image.py
@@ -1,10 +1,12 @@
 from tkinter import *
 
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageOps
 
 import os 
 
 root = Tk()
+
+root.title('Cadastro')
 
 #Lista os arquivos da pasta imagens
 arquivos = os.listdir('imagens')
@@ -12,10 +14,15 @@ arquivos = os.listdir('imagens')
 #Variavel para armazenar as imagens
 imagens = []
 
+#Variavel de controle de indices de imagem atual
+imagem_atual = 0
+
 #Percorer a lista de arquivos
 for arquivo in arquivos:
     #Abre a imagem
     img = Image.open('imagens/' + arquivo)
+    #Redimencionar imagem
+    img = ImageOps.contain(img, (300, 200))
     #Adiciona a imagem a lista
     imagens.append(ImageTk.PhotoImage(img))
 
@@ -25,8 +32,60 @@ for arquivo in arquivos:
 #img = Image.open('imagens/meme.jpg')
 #img_tk = ImageTk.PhotoImage(img)
 
-img_label = Label(root, image=imagens [0])
+img_label = Label(root, image=imagens[imagem_atual])
 
-img_label.pack()
+img_label.grid(column=0 , row=0, columnspan=3)
+
+def prev_Image():
+    global imagem_atual
+    global img_label
+    global imagens
+
+#verifica se e a primeira imagem. se sim, volta para a ultima imagem
+    if imagem_atual == 0:
+        imagem_atual = len(imagens) -1
+    else:
+        imagem_atual -= 1
+
+   #apaga a imagem atual 
+    img_label.grid_forget()
+
+   #exibe a nova imgame
+    img_label = Label(root, image=imagens[imagem_atual])
+    img_label.grid(column=0, row=0, columnspan=3)
+
+def next_Image():
+
+    global imagem_atual
+    global img_label
+    global imagens
+
+#verifica se e a primeira imagem. se sim, volta para a ultima imagem
+    if imagem_atual == len(imagens) -1:
+        imagem_atual = 0
+    else:
+        imagem_atual += 1
+
+   #apaga a imagem atual 
+    img_label.grid_forget()
+
+   #exibe a nova imgame
+    img_label = Label(root, image=imagens[imagem_atual])
+    img_label.grid(column=0, row=0, columnspan=3)
+
+    pass
+
+#Cria botao 
+botao = Button(root, text='ANTERIOR', command=prev_Image)
+
+botao.grid(column=0 , row=1)
+
+botao = Button(root, text='PROXIMO', command=next_Image)
+
+botao.grid(column=1 , row=1)
+
+botao = Button(root, text='SAIR', command=root.quit)
+
+botao.grid(column=2 , row=1)
 
 root.mainloop()
